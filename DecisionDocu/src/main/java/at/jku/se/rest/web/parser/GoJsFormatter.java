0a674a8630e49f5322ef9f9f3abd8aeecfe9ccc4
Peter Aichinger <aichinger.p@gmx.at>
2015-12-28 21:42:13 +0100

    Allow to add and edit influence factors, rationales, consequences and alternatives in the decision graph

--- DecisionDocu/src/main/java/at/jku/se/rest/web/parser/GoJsFormatter.java
+++ DecisionDocu/src/main/java/at/jku/se/rest/web/parser/GoJsFormatter.java
@@ -27,7 +27,7 @@ public class GoJsFormatter {
 	
 	public GoJsFormatter(WebDecision decision){
 			String decisionAsJsonString = "[";
-			decisionAsJsonString += "{\"key\": 0, \"text\": \"" + decision.getName() + "\", \"loc\": \"0 0\"}";
+			decisionAsJsonString += "{\"key\": 0, \"text\": \"" + decision.getName() + "\", \"loc\": \"0 0\", \"editable\": false, \"showAdd\": false}";
 			// configuration
 			int distanceRight = decision.getName().length() * 7;
 			int distanceLeft = 50;
@@ -46,35 +46,35 @@ public class GoJsFormatter {
 			// distance in pixel - y-axes
 			int distanceFactor = 50;
 			// add influences
-			decisionAsJsonString += ",{\"key\": 1,\"parent\":0,\"text\":\"Influence Factors\", \"brush\": \"skyblue\", \"dir\": \"right\", \"loc\": \"" + distanceRight + " -22\"}";
+			decisionAsJsonString += ",{\"key\": 1,\"parent\":0,\"text\":\"Influence Factors\", \"brush\": \"skyblue\", \"dir\": \"right\", \"loc\": \"" + distanceRight + " -22\",\"editable\": false, \"showAdd\": true}";
 			int key = 11;
 			for(InfluenceFactor i : decision.getInfluenceFactors()){
-				decisionAsJsonString += ",{\"key\": " + key + ",\"parent\":1,\"text\":\"" + i.getName() + "\", \"brush\": \"skyblue\", \"dir\": \"right\", \"loc\": \"" + (distanceRight + 250) + " " + (rightElementsCount * -1 * distanceFactor) + "\"}";
+				decisionAsJsonString += ",{\"key\": " + key + ",\"parent\":1,\"text\":\"" + i.getName() + "\", \"brush\": \"skyblue\", \"dir\": \"right\", \"loc\": \"" + (distanceRight + 250) + " " + (rightElementsCount * -1 * distanceFactor) + "\",\"editable\": true, \"showAdd\": false}";
 				rightElementsCount--;
 				key++;				
 			}
 			
 			// add rationals
-			decisionAsJsonString += ",{\"key\": 2,\"parent\":0,\"text\":\"Rationals\", \"brush\": \"darkseagreen\", \"dir\": \"right\", \"loc\": \"" + distanceRight + " 43\"}";
+			decisionAsJsonString += ",{\"key\": 2,\"parent\":0,\"text\":\"Rationals\", \"brush\": \"darkseagreen\", \"dir\": \"right\", \"loc\": \"" + distanceRight + " 43\",\"editable\": false, \"showAdd\": true}";
 			key = 21;
 			for(Rationale r : decision.getRationales()){
-				decisionAsJsonString += ",{\"key\": " + key + ",\"parent\":2,\"text\":\"" + r.getName() + "\", \"brush\": \"darkseagreen\", \"dir\": \"right\", \"loc\": \"" + (distanceRight + 250) + " " + (rightElementsCount * -1 * distanceFactor) + "\"}";
+				decisionAsJsonString += ",{\"key\": " + key + ",\"parent\":2,\"text\":\"" + r.getName() + "\", \"brush\": \"darkseagreen\", \"dir\": \"right\", \"loc\": \"" + (distanceRight + 250) + " " + (rightElementsCount * -1 * distanceFactor) + "\",\"editable\": true, \"showAdd\": false}";
 				rightElementsCount--;
 				key++;				
 			}
 			// add alternatives
-			decisionAsJsonString += ",{\"key\": 3,\"parent\":0,\"text\":\"Alternatives\", \"brush\": \"palevioletred\", \"dir\": \"left\", \"loc\": \"" + distanceLeft * (-1) + " -50\"}";
+			decisionAsJsonString += ",{\"key\": 3,\"parent\":0,\"text\":\"Alternatives\", \"brush\": \"palevioletred\", \"dir\": \"left\", \"loc\": \"" + distanceLeft * (-1) + " -50\",\"editable\": false, \"showAdd\": true}";
 			key = 31;
 			for(Alternative a : decision.getAlternatives()){
-				decisionAsJsonString += ",{\"key\": " + key + ",\"parent\":3,\"text\":\"" + a.getName() + "\", \"brush\": \"palevioletred\", \"dir\": \"left\", \"loc\": \"" + ((distanceLeft * -1) - 250) + " " + (leftElementsCount * -1 * distanceFactor) + "\"}";
+				decisionAsJsonString += ",{\"key\": " + key + ",\"parent\":3,\"text\":\"" + a.getName() + "\", \"brush\": \"palevioletred\", \"dir\": \"left\", \"loc\": \"" + ((distanceLeft * -1) - 250) + " " + (leftElementsCount * -1 * distanceFactor) + "\",\"editable\": true, \"showAdd\": false}";
 				leftElementsCount--;
 				key++;				
 			}
 			// add consequences
-			decisionAsJsonString += ",{\"key\": 4,\"parent\":0,\"text\":\"Consequences\", \"brush\": \"coral\", \"dir\": \"left\", \"loc\": \"" + distanceLeft * (-1) + " 50\"}";
+			decisionAsJsonString += ",{\"key\": 4,\"parent\":0,\"text\":\"Consequences\", \"brush\": \"coral\", \"dir\": \"left\", \"loc\": \"" + distanceLeft * (-1) + " 50\",\"editable\": false, \"showAdd\": true}";
 			key = 41;
 			for(Alternative a : decision.getAlternatives()){
-				decisionAsJsonString += ",{\"key\": " + key + ",\"parent\":4,\"text\":\"" + a.getName() + "\", \"brush\": \"coral\", \"dir\": \"left\", \"loc\": \"" + ((distanceLeft * -1) - 250) + " " + (leftElementsCount * -1 * distanceFactor) + "\"}";
+				decisionAsJsonString += ",{\"key\": " + key + ",\"parent\":4,\"text\":\"" + a.getName() + "\", \"brush\": \"coral\", \"dir\": \"left\", \"loc\": \"" + ((distanceLeft * -1) - 250) + " " + (leftElementsCount * -1 * distanceFactor) + "\",\"editable\": true, \"showAdd\": false}";
 				leftElementsCount--;
 				key++;				
 			}
