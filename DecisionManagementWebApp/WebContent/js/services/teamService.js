232b23244f84a6f26250485b3304069558ce75fe
thomas_hochgatterer <thomas.hochgatterer@enjoyit.at>
2015-12-29 10:10:51 +0100

    Team selection after login

--- DecisionManagementWebApp/WebContent/js/services/teamService.js
+++ DecisionManagementWebApp/WebContent/js/services/teamService.js
@@ -3,9 +3,31 @@
  */
 var teamServices = angular.module('teamServices', ['ngResource']);
 
-teamServices.factory('Teams', ['$resource', 
-                                   function($resource, $rootScope){
-                                     return $resource(serverAddress + '/DecisionManagement/rest/team/', {}, {
-                                       query: {method:'GET', params:{}, isArray:true}
-                                     });
+teamServices.factory('TeamPerId', ['$resource', '$cookies',
+                                     function ($resource, $cookies) {
+                                         return $resource(serverAddress + '/DecisionDocu/api/web/team/:teamId', {teamId:'@id'} , {
+                                         	 get: {
+                                         		 	isArray: false,
+                                         	        headers: {
+                                         	            'token': $cookies['Token']
+                                         	        }
+                                         	    }
+                                         });
+                                     }]);
+
+
+teamServices.factory('Teams', ['$resource', '$cookies',
+                                   function ($resource, $cookies) {
+                                       return $resource(serverAddress + '/DecisionDocu/api/web/team', {} , {
+                                       	 get: {
+                                       		 	isArray: true,
+                                       	        headers: {
+                                       	            'token': $cookies['Token']
+                                       	        }
+                                       	    }
+                                       });
                                    }]);
+
+
+
+
