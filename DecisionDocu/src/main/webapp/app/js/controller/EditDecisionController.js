589b74a3316fd3a9caba9da892ad3ce065b536b6
Peter Aichinger <aichinger.p@gmx.at>
2015-12-31 15:47:03 +0100

    EditDecision - edit attribute categories

--- DecisionDocu/src/main/webapp/app/js/controller/EditDecisionController.js
+++ DecisionDocu/src/main/webapp/app/js/controller/EditDecisionController.js
@@ -1,44 +1,114 @@
-app.controller('EditDecisionController', ['$scope', '$cookies', 'LoadEditGraph', 'DecisionsByTeam', function ($scope, $cookies, LoadEditGraph, DecisionsByTeam) {
-        $scope.model = new go.GraphLinksModel([], []);
+app.controller('EditDecisionController', [
+		'$scope',
+		'$cookies',
+		'LoadEditGraph',
+		'DecisionsByTeam',
+		'AddAttributeToDecision',
+		function($scope, $cookies, LoadEditGraph, DecisionsByTeam,
+				AddAttributeToDecision) {
+			// init attributes
+			$scope.attributes = [ {
+				value : 'addAlternative',
+				label : '#Alternative'
+			}, {
+				value : 'addConsequence',
+				label : '#Consequence'
+			}, {
+				value : 'addInfluenceFactor',
+				label : '#Influence Factor'
+			}, {
+				value : 'addRationale',
+				label : '#Rationale'
+			}, {
+				value : 'addQualityAttribute',
+				label : '#Quality Attribute'
+			} ];
+			// init attribute categories values
+			$scope.attributeValues = [];
+			
+			// init decision array
+			$scope.decisions = [];
+			// init go-js-model
+			$scope.model = new go.GraphLinksModel([], []);
+			$scope.model.selectedNodeData = null;
+			$scope.selectedDecisionId = "";
+			$scope.selectedAttribute = "";
+			// load all graphs from team to show in selection
+			DecisionsByTeam.get({}, function(data) {
+				var obj = angular.fromJson(data);
+				var firstElementId = "";
+				for (var i = 0; i < obj.length; i++) {
+					if (i == 0) {
+						firstElementId = obj[i].id;
+						$scope.selectedDecisionId = obj[i].id;
+					}
+					$scope.decisions.push({
+						"id" : obj[i].id,
+						"name" : obj[i].name,
+						"alternatives" : obj[i].alternatives
+					});
+					alert("alternatives count in decision: " + obj[i].alternatives.length);
+					alert("id of first alternative: " + obj[i].alternatives[0].id);
+				}
+				if (firstElementId != "") {
+					// load the first graph
+					LoadEditGraph.get({
+						id : firstElementId
+					}, function(data) {
+						// alert(data);
+						$scope.model = new go.Model.fromJson({
+							"class" : "go.TreeModel",
+							"nodeDataArray" : data
+						}), $scope.model.selectedNodeData = null;
+					}, function(error) {
+					});
+				}
+			}, function(error) {
+			});
 
-        $scope.updateGraph = function() {
-        	   alert($scope.selectedDecision);
-               // load chosen graph
-               LoadEditGraph.get({id: $scope.selectedDecision}, function (data) {
-                //alert(data);
-                   $scope.model = new go.Model.fromJson({"class": "go.TreeModel","nodeDataArray": data}),
-                   $scope.model.selectedNodeData = null;
-               }, function (error) {
-               });
-        	}
-        $scope.model.selectedNodeData = null;
-        $scope.decisions = [];
-        // load all graphs from team
-        DecisionsByTeam.get({}, function (data) {
-        	var obj = angular.fromJson(data);
-        	var firstElementId = "";
-        	for(var i = 0; i< obj.length;i++){
-        		if(i == 0){
-        			firstElementId = obj[i].id;
-        		}
-				$scope.decisions.push({
-					"id" : obj[i].id,
-					"name" : obj[i].name
+			// function to update selected graph - draw selected graph
+			$scope.updateGraph = function() {
+				alert($scope.selectedDecision);
+				// load chosen graph
+				LoadEditGraph.get({
+					id : $scope.selectedDecision
+				}, function(data) {
+					// alert(data);
+					$scope.model = new go.Model.fromJson({
+						"class" : "go.TreeModel",
+						"nodeDataArray" : data
+					}), $scope.model.selectedNodeData = null;
+				}, function(error) {
 				});
-        		//alert(obj[i].id);
-        	}
-        	if(firstElementId != ""){
-                // load the first graph
-                LoadEditGraph.get({id: firstElementId}, function (data) {
-                 //alert(data);
-                    $scope.model = new go.Model.fromJson({"class": "go.TreeModel","nodeDataArray": data}),
-                    $scope.model.selectedNodeData = null;
-                }, function (error) {
-                });
-        	}
-           }, function (error) {
-           });
-
-    }]);
-
-
+			}
+			// load attributes from chosen category
+			$scope.changeAttributeCategories = function() {
+				alert("Attribute choose changed" + $scope.selectedAttribute);
+				/*$scope.decisions.push({
+					"id" : obj[i].id,
+					"name" : obj[i].name,
+					"alternatives" : obj[i].alternatives
+				});*/
+				$scope.attributeValues.push({value: "test",label: "test"});
+			}
+			// load attributes category values to edit
+			$scope.changeEditAttributeValues = function() {
+				alert("Attribute value choose changed: " + $scope.selectedAttributeValue);
+				
+			}
+			// function to add attributes to decision
+			$scope.addDecisionAttribute = function() {
+				//alert($scope.selectedDecisionId);
+				//alert($scope.selectedAttribute);
+				alert($scope.attributeValue);
+				AddAttributeToDecision.save({
+					value : $scope.attributeValue,
+					id : $scope.selectedDecisionId,
+					attribute : $scope.selectedAttribute
+				}, function(data) {
+					alert(data);
+					$scope.updateGraph();
+				}, function(error) {
+				});
+			}
+		} ]);
--- DecisionDocu/src/main/webapp/app/js/services/decisionService.js
+++ DecisionDocu/src/main/webapp/app/js/services/decisionService.js
@@ -1,39 +1,69 @@
 /**
  * service for the authentification
  */
-var decisionServices = angular.module('decisionServices', ['ngResource', 'ngCookies']);
+var decisionServices = angular.module('decisionServices', [ 'ngResource',
+		'ngCookies' ]);
 
+decisionServices.factory('LoadEditGraph', [
+		'$resource',
+		'$cookies',
+		function($resource, $cookies) {
+			return $resource(serverAddress
+					+ '/DecisionDocu/api/web/decision/getGraphAsJsonById', {},
+					{
+						get : {
+							isArray : true,
+							headers : {
+								'token' : $cookies['Token']
+							}
+						}
+					});
+		} ]);
 
-decisionServices.factory('LoadEditGraph', ['$resource', '$cookies',
-    function ($resource, $cookies) {
-        return $resource(serverAddress + '/DecisionDocu/api/web/decision/getGraphAsJsonById', {}, {
-        	 get: {
-        		 	isArray: true,
-        	        headers: {
-        	            'token': $cookies['Token']
-        	        }
-        	    }
-        });
-    }]);
-
-decisionServices.factory('LoadConnectionsGraph', ['$resource', '$cookies',
-                                           function ($resource, $cookies) {
-                                               return $resource(serverAddress + '/DecisionDocu/api/web/decision/getTeamGraphsForConnectionAsJsonByTeamId', {}, {
-                                               	 get: {
-                                               	        headers: {
-                                               	            'token': $cookies['Token']
-                                               	        }
-                                               	    }
-                                               });
-                                           }]);
-decisionServices.factory('DecisionsByTeam', ['$resource', '$cookies',
-                                                  function ($resource, $cookies) {
-                                                      return $resource(serverAddress + '/DecisionDocu/api/web/decision/byTeam/' + $cookies['TeamId'], {}, {
-                                                      	 get: {
-                                                      		 	isArray: true,
-                                                      	        headers: {
-                                                      	            'token': $cookies['Token']
-                                                      	        }
-                                                      	    }
-                                                      });
-                                                  }]);
+decisionServices
+		.factory(
+				'LoadConnectionsGraph',
+				[
+						'$resource',
+						'$cookies',
+						function($resource, $cookies) {
+							return $resource(
+									serverAddress
+											+ '/DecisionDocu/api/web/decision/getTeamGraphsForConnectionAsJsonByTeamId',
+									{}, {
+										get : {
+											headers : {
+												'token' : $cookies['Token']
+											}
+										}
+									});
+						} ]);
+decisionServices.factory('DecisionsByTeam', [
+		'$resource',
+		'$cookies',
+		function($resource, $cookies) {
+			return $resource(serverAddress
+					+ '/DecisionDocu/api/web/decision/byTeam/'
+					+ $cookies['TeamId'], {}, {
+				get : {
+					isArray : true,
+					headers : {
+						'token' : $cookies['Token']
+					}
+				}
+			});
+		} ]);
+decisionServices.factory('AddAttributeToDecision', [
+		'$resource',
+		'$cookies',
+		function($resource, $cookies) {
+			return $resource(serverAddress 
+					+ '/DecisionDocu/api/web/decision/:decisionId/:attribute?value=:value',{decisionId:'@id',attribute : '@attribute',value : '@value'}, {
+						save : {
+							method : 'POST',
+							headers : {
+								'token' : $cookies['Token']
+							}
+						}
+					});
+		} ]);
