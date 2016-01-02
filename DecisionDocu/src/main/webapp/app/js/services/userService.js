5f05ee8d82ebe74776d1c3cdbe5ff2fd726a4cc2
thomas_hochgatterer <thomas.hochgatterer@enjoyit.at>
2016-01-02 19:09:13 +0100

    display profile picture from Base64 Code

--- DecisionDocu/src/main/webapp/app/js/services/userService.js
+++ DecisionDocu/src/main/webapp/app/js/services/userService.js
@@ -1,26 +1,48 @@
 /**
  * service for user information
  */
-var userServices = angular.module('userServices', ['ngResource']);
+var userServices = angular.module('userServices', [ 'ngResource' ]);
 
-userServices.factory('Users', ['$resource',
-    function ($resource) {
-        return $resource(serverAddress + '/DecisionManagement/rest/user/', {}, {
-            query: {method: 'GET', params: {}, isArray: true}
-        });
-    }]);
+userServices.factory('Users', [ '$resource', function($resource) {
+	return $resource(serverAddress + '/DecisionManagement/rest/user/', {}, {
+		query : {
+			method : 'GET',
+			params : {},
+			isArray : true
+		}
+	});
+} ]);
 
+userServices.factory('UserPerMail', [
+		'$resource',
+		'$cookies',
+		function($resource, $cookies) {
+			return $resource(
+					serverAddress + '/DecisionDocu/api/web/user/:mail', {
+						mail : '@id'
+					}, {
+						get : {
+							isArray : false,
+							headers : {
+								'token' : $cookies['Token']
+							}
+						}
+					});
+		} ]);
 
-userServices.factory('UserPerMail', ['$resource', '$cookies',
-                                       function ($resource, $cookies) {
-                                           return $resource(serverAddress + '/DecisionDocu/api/web/user/:mail', {mail:'@id'}, {
-                                           	 get: {
-                                           		 	isArray: false,
-                                           	        headers: {
-                                           	            'token': $cookies['Token']
-                                           	        }
-                                           	    }
-                                           });
-                                       }]);
-
-
+userServices.factory('UserPicture', [
+		'$resource',
+		'$cookies',
+		function($resource, $cookies) {
+			return $resource(serverAddress + '/DecisionDocu/api/web/user/getFile/:id', {
+				id : '@id'
+			}, {
+				get : {
+					method: "GET",
+					isArray: false,
+					headers : {
+						'token' : $cookies['Token']
+					}
+				}
+			});
+		} ]);
