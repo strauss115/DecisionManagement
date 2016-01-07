170fb7f4275f56622e05bb1aaeb63b36db1fe5ae
Peter Aichinger <aichinger.p@gmx.at>
2016-01-07 15:22:18 +0100

    add connect to decision page

--- DecisionDocu/src/main/webapp/app/js/controller/RelateDecisionController.js
+++ DecisionDocu/src/main/webapp/app/js/controller/RelateDecisionController.js
@@ -1,14 +1,33 @@
-app.controller('RelateDecisionController', ['$scope', '$cookies', 'LoadConnectionsGraph', function ($scope, $cookies, LoadConnectionsGraph) {
-    $scope.model = new go.GraphLinksModel([], []);
-    $scope.model.selectedNodeData = null;
-
-    LoadConnectionsGraph.get({teamId: "6141"}, function (data) {
-    	alert(data);
-        $scope.model = new go.GraphLinksModel(data['data'],data['relations']);
-        $scope.model.selectedNodeData = null;
-    }, function (error) {
-    });
-}]);
-
-
-
+app.controller('RelateDecisionController', [
+		'$scope',
+		'$cookies',
+		'LoadConnectionsGraph',
+		'DecisionsByTeam',
+		function($scope, $cookies, LoadConnectionsGraph,DecisionsByTeam) {
+			$scope.model = new go.GraphLinksModel([], []);
+			$scope.model.selectedNodeData = null;
+			// init decision array
+			$scope.decisions = [];
+			LoadConnectionsGraph.get({
+				teamId : $cookies['TeamId']
+			}, function(data) {
+				alert(data);
+				$scope.model = new go.GraphLinksModel(data['data'],
+						data['relations']);
+				$scope.model.selectedNodeData = null;
+			}, function(error) {
+			});
+			// load all graphs from team to show in selection
+			DecisionsByTeam.get({}, function(data) {
+				var obj = angular.fromJson(data);
+				var firstElementId = "";
+				for (var i = 0; i < obj.length; i++) {
+					$scope.decisions.push({
+						"id" : obj[i].id,
+						"name" : obj[i].name,
+						"alternatives" : obj[i].alternatives
+					});
+				}
+			}, function(error) {
+			});
+		} ]);
